c703d28eec97f621167450d969783c63656d8392
gponciano <gab_bash@hotmail.com>
2025-02-22 11:40:10 +1100

    Creating project, adding header component UI

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import styles from './App.module.css';
+import './global.css'
+
+import { NavigationBar } from './components/NavigationBar'
+
+function App() {
+  
+
+  return (
+   <NavigationBar />
+  )
+}
+
+export default App
